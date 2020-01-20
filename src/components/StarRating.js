090bd1eccaa7cc67fb47b1808c587b7156f2be1e
Chris Kalfas <christopherkalfas22@gmail.com>
2020-01-20 12:21:49 -0500

    imported imgs for courses. also, created state for a star rating of a course and added handlers to update the state of the stars and render the update

--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -3,20 +3,47 @@ import Star from './Star';
 
 class StarRating extends Component {
 
+    state = {
+        rating: 0
+    }
   // Initialize a 'rating' state
 
 
   // Write a function that returns 5 Star components
+renderStars = () => {
+    let stars = []
+    let maxRating = 5 
 
+    for (let i = 0; i < maxRating; i ++) {
+        stars.push(
+            <Star
+                isSelected={ this.state.rating > i } 
+                setRating= {() => this.handleStarUpdate(i + 1) }
+                key={i}
+            />
+        )
+    }
+    return stars
+}
 
   // Write an event handler that updates the rating state.
+
+handleStarUpdate = (rating) => {
+    if (this.state.rating === rating) {
+        this.setState({ rating : 0 })
+    } else {
+        this.setState(
+            { rating }
+        )
+    }
+}
   // Pass the function to a Star component via props
 
 
     render() {
         return (
             <ul className="course--stars">
-                {/* Render the Star components */}
+                {this.renderStars()}
             </ul>
         );
     }
